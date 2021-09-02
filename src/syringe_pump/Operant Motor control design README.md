99a8a4ed861b6ad25997c109704fb695b4ff982a
shoyag-noto <shoyag@gmail.com>
2021-09-02 19:12:25 +0900

    フォルダ再構成

---
+++ src/syringe_pump/Operant Motor control design README.md
@@ -0,0 +1,47 @@
+
+Arduino Operant Motor Control Design (05/31/2021)
+
+High Level Architecture design (1st phase, current code design) : 
+
+1. Initial parameters setup
+2. Initial system setup 
+3. Loop creation
+    (add parameter for counting)  
+    1. Send message asking for nose poke touch? 
+    1. If receive "yes", check initial parameter, if it is first time, 
+        Do One push action 
+        Do One pull action
+        Counter increment  
+    1. go back to 3-1, look for another nose poke touch? 
+    1. If receive "yes", check parameter, if it is second time, 
+        Do One New push action 
+        Do One pull action
+        Counter increment 
+        go back to 3-1, and repeat 
+    1. If counter reaches "one defined number" 
+    1. Return 0 (finish the program, no water) 
+
+Software allows parameter changes for 
+
+1. Number of evolution                             
+1. Number of steps per second (RPM) (Revolution per minute)
+
+High Level Architecture design (2nd phase) : 
+
+"Send message" portion can be changed to "nose poke" detection for automotic nose poke detection 
+"Noke poke sensor can be connected to Arduino digital input to verify this detection and water supply
+
+
+High Level Architecture design (3rd phase) : 
+
+Integrate control from Python/Pycharm (PC or Raspberry Pi) 
+
+One motor version : 
+
+1. Bring up serial (USB) 
+2. Send "Ready" to Host
+3. Ask to start the work to Host? 
+4. If Y, then start
+5. continue polling, if N, then stop 
+6. Once nose poke detects and water is supplied, send notification message to Host (no ack is needed)
+7. Python continues to collect notification message for log collection (Python can measure time when it receive the notification)
---
+++ src/syringe_pump/_archive/arduino_syringe_motor_v0.3_auto_test.ino
@@ -0,0 +1,118 @@
+
+// Syringe Pump motor control rev0.2 06/08/2021  Chon-wa Cheong
+
+// Rev History 
+// Rev0.1 6/1/2021 : Initial code
+// Rev0.2 6/8/2021 : Correct Motor parameter setting 
+// Rev0.3 6/16/2021 : Add external pin D0 as detection for water
+
+#include <AccelStepper.h> 
+
+#define X_SPEED 64      // X steps pull position 
+#define X_DOUBLE 158    // X steps push position 
+#define X_ACCEL 8       // no meaning 
+
+#define EN        8    
+
+#define X_DIR     5    // direction pin definition 
+#define X_STP     2  // step pin definition 
+
+#define BAUD_RATE 9600 // define serial baud rate 
+
+AccelStepper stepper(AccelStepper::DRIVER, X_STP, X_DIR); // define motor driver mode 
+
+int k; 
+int i;
+int j;
+int l; 
+int m;
+int val;
+char x;
+
+
+void setup() {
+  pinMode (3, INPUT); 
+  pinMode (8, OUTPUT); 
+  Serial.begin(BAUD_RATE);
+  stepper.setMaxSpeed(500);   //max speed steps per second settiing 
+  stepper.setAcceleration(6000); // acceleration steps per second per second 
+  stepper.setSpeed(500);  // speed setting steps per second 
+  
+  // stepper.setEnablePin(8); 
+
+  Serial.println("<Adruino is ready");
+  Serial.println("<Nose Poke touch? Y/N>");
+  k = 0 ;
+  val = 0; 
+  }
+
+void loop() {
+  
+   // if (Serial.available()>0) {
+   // char x= Serial.read();
+
+   // stepper.enableOutputs(); 
+   // stepper.disableOutputs();
+
+      val = digitalRead(3); 
+   
+    if (val == LOW && k < 100 ) {
+
+     // if (x == 'Y' && k == 0) {
+     // i = stepper.currentPosition(); 
+     // stepper.moveTo(X_SPEED + i );  // set target position + current position
+     // while (stepper.currentPosition() != (i + X_SPEED)) // Full speed up to 200 + current position 
+     // stepper.run();
+     // stepper.stop();
+     // delay(1000);
+
+     //j = stepper.currentPosition();    
+     // stepper.moveTo(j - X_SPEED ); // set new target position as current position - 200 
+     // while (stepper.currentPosition() != j - X_SPEED) // Full speed back
+     // stepper.run();
+     // stepper.stop();     
+     // Serial.println("water provided, Nose Poke touch again? Y/N? \n");
+     // k++ ; 
+     // }
+     
+        digitalWrite(8, LOW); 
+    i = stepper.currentPosition(); 
+      	stepper.moveTo(X_DOUBLE + i );  // set target position + current position
+        while (stepper.currentPosition() != (i + X_DOUBLE )) // Full speed up to 400 + current position
+      	stepper.run();
+        stepper.stop();
+      	delay(500);
+        
+    j = stepper.currentPosition();
+        stepper.moveTo( j - X_SPEED ); // set new target position
+        while (stepper.currentPosition() != j - X_SPEED ) // Full speed back  
+   	    stepper.run();
+        stepper.stop();
+        delay(500);
+        k++;
+     }
+     
+     // Serial.println("water provided 2nd time, Nose Poke touch again? Y/N? \n");
+       
+      else {
+        digitalWrite(8, HIGH);
+     // Serial.println (val);
+        
+      }
+
+    }
+ 
+    //else if ( x == 'N' ) {
+    //  Serial.println("system stops and move back to initial position \n"); 
+    //  k = 0;
+    // l = stepper.currentPosition();
+    //  stepper.moveTo ( 0 - l ); // move back to the origitnal position
+    //  while (stepper.currentPosition() != 0 - l) 
+    //  stepper.run();
+    //  stepper.stop();
+    // Serial.println ("back to original position \n");
+    //  }
+    
+
+  
+  
