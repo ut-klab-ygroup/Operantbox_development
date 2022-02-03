d0780b0ed423a6c12bc784a6a6144f5810635a67
shoyagishita <yagishita-tky@umin.ac.jp>
2022-02-03 13:01:30 +0900

    Update arduino_syringe_motor_water_amount_test.ino

--- src/syringe_pump/arduino_syringe_motor_water_amount_test/arduino_syringe_motor_water_amount_test.ino
+++ src/syringe_pump/arduino_syringe_motor_water_amount_test/arduino_syringe_motor_water_amount_test.ino
@@ -3,17 +3,17 @@
 
 
 
-#define PUSH_POSITION_DELTA 200    // X steps push position 
-#define PULL_POSITION_DELTA 150      // X steps pull position 
-#define ACCEL 2000
-#define SPEED 500
+#define PUSH_POSITION_DELTA 20    // X steps push position 
+#define PULL_POSITION_DELTA 15      // X steps pull position 
+#define ACCEL 50
+#define SPEED 100
 #define PUSH_MAX 100    // X steps push position 
 
 #define X_DIR     5    // direction pin definition 
 #define X_STP     2  // step pin definition 
 
 #define BAUD_RATE 9600 // define serial baud rate 
-
+#define DEBUG true
 AccelStepper stepper(AccelStepper::DRIVER, X_STP, X_DIR); // define motor driver mode
 
 int k;      // Define the max number of times Motor can run until it gets stop
@@ -25,11 +25,9 @@ int val;    // GPIO Trigger signal read from GPIO#3
 
 void setup() {
   pinMode (3, INPUT); // Input triggering for water, 5V water out, 0V stop
-  pinMode (8, OUTPUT);  // used for test purpose only
+
   Serial.begin(BAUD_RATE);
-  stepper.setMaxSpeed(SPEED);   //max speed steps per second settiing
-  stepper.setAcceleration(ACCEL); // acceleration steps per second
-  stepper.setSpeed(SPEED);  // speed setting steps per second
+
 
   k = 0 ;
   val = 0;
@@ -39,23 +37,45 @@ void loop() {
 
 
   while (digitalRead(3) == LOW) {
-    delay(100);
+    delay(500);
   }
-
+  // setting pinmode triggers electrical outputs, thereby heating up the motor.
+  pinMode (8, OUTPUT);  // used for test purpose only
+  stepper.setMaxSpeed(SPEED);   //max speed steps per second settiing
+  stepper.setAcceleration(ACCEL); // acceleration steps per second
+  stepper.setSpeed(SPEED);  // speed setting steps per second
   for (k = 0; k < PUSH_MAX; k++) {
 
     digitalWrite(8, LOW);
+    if (DEBUG) {
+      Serial.println("current");
+      Serial.println(k);
+      Serial.println(stepper.currentPosition());
+    }
+
     i = stepper.currentPosition() - PUSH_POSITION_DELTA;
-    stepper.runToNewPosition(i);
-    while (stepper.currentPosition() != i  ) // Full speed back
-      delay(50);
-    delay(500);
-    j = stepper.currentPosition() + PULL_POSITION_DELTA;
-    stepper.runToNewPosition(j);
-    while (stepper.currentPosition() != j  ) // Full speed back
-      delay(50);
+    if (DEBUG) {
+      Serial.println("push");
+      Serial.println(i);
+    }
+
+    stepper.moveTo( i ); // set new target position
+    while (stepper.currentPosition() != i   ) // Full speed back
+      stepper.run();
 
     delay(500);
+    j = stepper.currentPosition() + PULL_POSITION_DELTA;
+    if (DEBUG) {
+      Serial.println("pull");
+      Serial.println(j);
+    }
+
+    stepper.moveTo( j ); // set new target position
+    while (stepper.currentPosition() != j   ) // Full speed back
+      stepper.run();
+
+    delay(500);
+
   }
 
 }
