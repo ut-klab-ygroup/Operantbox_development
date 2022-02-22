328bb4e1629685283e288027c29f3b6a63c738cd
chcheong2000 <83069601+chcheong2000@users.noreply.github.com>
2022-02-22 18:15:31 +0900

    Update arduino_syringe_motor_pullback.ino

--- src/syringe_pump/arduino_syringe_motor_pullback/arduino_syringe_motor_pullback.ino
+++ src/syringe_pump/arduino_syringe_motor_pullback/arduino_syringe_motor_pullback.ino
@@ -1,9 +1,9 @@
 #include <AccelStepper.h>
 #include <MultiStepper.h>
 
-#define PULL_POSITION_DELTA 1000    // X steps pull position 
-#define ACCEL 60000
-#define SPEED 5000
+#define PULL_POSITION_DELTA 10000    // X steps pull position 
+#define ACCEL 3000
+#define SPEED 1000
 
 #define X_DIR     5    // direction pin definition 
 #define X_STP     2  // step pin definition 
@@ -12,6 +12,7 @@
 
 AccelStepper stepper(AccelStepper::DRIVER, X_STP, X_DIR); // define motor driver mode
 
+char incomingByte = 0; // for incoming serial data 
 int k;      // Define the max number of times Motor can run until it gets stop
 int i;      // Define Initial Motor Current position
 int j;      // Define Motor current position after push (in order to define pull action)
@@ -22,9 +23,9 @@ void setup() {
   pinMode (3, INPUT); // Input triggering for water, 5V water out, 0V stop
   pinMode (8, OUTPUT);  // Motor enable/disable control 
   Serial.begin(BAUD_RATE);
-  stepper.setMaxSpeed(5000);   //max speed steps per second settiing
-  stepper.setAcceleration(60000); // acceleration steps per second
-  stepper.setSpeed(5000);  // speed setting steps per second
+  stepper.setMaxSpeed(SPEED);   //max speed steps per second settiing
+  stepper.setAcceleration(ACCEL); // acceleration steps per second
+  stepper.setSpeed(SPEED);  // speed setting steps per second
 
   // stepper.setEnablePin(8);
   digitalWrite(8, HIGH);  // disable motor
@@ -36,10 +37,10 @@ void loop() {
   if (Serial.available() > 0) {
     // read incoming bytes
     incomingByte = Serial.read();
-    Serial.print ("Please enter "p" to pullback");
+    Serial.print ("Please enter p to pullback");
     stepper.setCurrentPosition(0);
     if (incomingByte == 'p') {
-      Serial.print ("Pull");
+      Serial.print("Pull");
       digitalWrite(8, LOW); // Enable motor
       stepper.setCurrentPosition(0);
   j = stepper.currentPosition() + PULL_POSITION_DELTA;
@@ -47,11 +48,11 @@ void loop() {
   while (stepper.currentPosition() != j ) // Full speed back
     stepper.run();
 
-  stepper.stop();
-  digitalWrite(8, HIGH); // disable motor
+  // stepper.stop();
+   digitalWrite(8, HIGH); // disable motor
     }
     else {
-      Serial.print ("wrong character, please enter again")
+      Serial.print("wrong character, please enter again");
     }
   }
 }
