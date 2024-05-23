96fb5c5e0a084e0a06c925374ba5ce7edd63c79e
takayoshio <takakmg@gmail.com>
2024-05-23 23:54:27 +0900

    phase2の実装, 未検証

--- src/operant_conditioning_task/states/nose_poke_state.py
+++ src/operant_conditioning_task/states/nose_poke_state.py
@@ -10,7 +10,7 @@ import time
 from transitions import State
 
 from states.task_result_enum import TaskResult
-
+from music import speaker
 
 class NosePokeState(State):
     """
@@ -77,58 +77,135 @@ class NosePokeState(State):
             self.results['state_result'] = TaskResult.Skipped
             self._logger.info(self.name + ': Skipped.')
             return
+        
+        if self._settings.current_trial_num == 2:
+            lick_time_list = []
+            nose_poke_time_list = []
+            nose_poke_hole_number_list = []
+            nose_poke_correct_time_list = []
+            nose_poke_hole_number_correct_list = []
+            while time.perf_counter() - start_time <= phase_settings.stimulus_duration_in_s:
+                if self._task_gpio.is_nose_poked:
+                    self._task_gpio.get_nose_poke_results(self.results)
+                    target_num = self._settings.NOSE_POKE_TARGETS[self.results['selected_index']]
+                    self.results['target_num'] = target_num
 
-        # 正解の nose poke ターゲットの LED を点灯します。
-        correct_target_index_list = self._settings.get_correct_target_index_list()
-        self._task_gpio.switch_nose_poke_leds('ON', correct_target_index_list)
+                    nose_poke_time_list.append(self.results['nose_poke_time'])
+                    nose_poke_hole_number_list.append(self.results['target_num'])
+                    self._logger.info(self.name + f'NP detected, NP{target_num}')
+                    
+                    """
+                    if self.results['selected_index'] in correct_target_index_list:
+                        self.results['is_correct'] = True
+                        self.results['state_result'] = TaskResult.Success
+                        self._logger.info(self.name + f': Correct nose poke ({target_num}). Success.')
+                    else:
+                        self.results['is_correct'] = False
+                        self.results['state_result'] = TaskResult.Failure
+                        self._logger.info(self.name + f': Incorrect nose poke ({target_num}). Failure.')
+                    """
+                    ##nose_poke_time_listに格納されているunix timeについて、今回のnose pokeの時間がnose_poke_correct_time_listの最新のものから5秒以上経っていたら、という条件分岐。もしくは、nose_poke_correct_time_listが空だったら。##
+                    if not nose_poke_correct_time_list or (self.results['nose_poke_time'] - nose_poke_correct_time_list[-1]) >= 5:
+                        nose_poke_correct_time_list.append(self.results['nose_poke_time'])
+                        nose_poke_hole_number_correct_list.append(self.results['target_num'])
+                        self._logger.info(self.name + f'NP correct onset, NP correct onset {target_num}')
+                    
+                        self._give_reward() 
+                    
+                time.sleep(0.001)
 
-        # LED 刺激呈示期間において、nose poke 課題を監視します。
-        # stimulus_duration_in_s = -1 の場合は、時間無制限です。
-        start_time = time.perf_counter()
-        while phase_settings.stimulus_duration_in_s == -1 or \
-                time.perf_counter() - start_time <= phase_settings.stimulus_duration_in_s:
-            if self._task_gpio.is_nose_poked:
-                self._task_gpio.get_nose_poke_results(self.results)
-                target_num = self._settings.NOSE_POKE_TARGETS[self.results['selected_index']]
-                self.results['target_num'] = target_num
-                if self.results['selected_index'] in correct_target_index_list:
-                    self.results['is_correct'] = True
-                    self.results['state_result'] = TaskResult.Success
-                    self._logger.info(self.name + f': Correct nose poke ({target_num}). Success.')
-                else:
-                    self.results['is_correct'] = False
-                    self.results['state_result'] = TaskResult.Failure
-                    self._logger.info(self.name + f': Incorrect nose poke ({target_num}). Failure.')
 
-                # Nose poke ターゲットの LED を消灯します。
-                self._task_gpio.switch_nose_poke_leds('OFF')
-                return
+                if self._task_gpio.is_licked:
+                    #この処理によって、resultsの中にlick_timeが格納される。
+                    self._task_gpio.get_lick_results(self.results)
+                    lick_time_list.append(self.results['lick_time'])
+                    #self.results['state_result'] = TaskResult.Success
+                    self._logger.info(self.name + 'Lick detected')
+                    
+                time.sleep(0.001)
+            
+            self.results['state_result'] = TaskResult.Success
+            self.results['lick_time_list'] = lick_time_list
+            self.results['nose_poke_time_list'] = nose_poke_time_list
+            self.results['nose_poke_hole_number_list'] = nose_poke_hole_number_list
+            self.results['nose_poke_correct_time_list'] = nose_poke_correct_time_list
+            self.results['nose_poke_hole_number_correct_list'] = nose_poke_hole_number_correct_list
+            
+        else:
+            # 正解の nose poke ターゲットの LED を点灯します。
+            correct_target_index_list = self._settings.get_correct_target_index_list()
+            self._task_gpio.switch_nose_poke_leds('ON', correct_target_index_list)
 
-            time.sleep(0.001)
+            # LED 刺激呈示期間において、nose poke 課題を監視します。
+            # stimulus_duration_in_s = -1 の場合は、時間無制限です。
+            start_time = time.perf_counter()
+            while phase_settings.stimulus_duration_in_s == -1 or \
+                    time.perf_counter() - start_time <= phase_settings.stimulus_duration_in_s:
+                if self._task_gpio.is_nose_poked:
+                    self._task_gpio.get_nose_poke_results(self.results)
+                    target_num = self._settings.NOSE_POKE_TARGETS[self.results['selected_index']]
+                    self.results['target_num'] = target_num
+                    if self.results['selected_index'] in correct_target_index_list:
+                        self.results['is_correct'] = True
+                        self.results['state_result'] = TaskResult.Success
+                        self._logger.info(self.name + f': Correct nose poke ({target_num}). Success.')
+                    else:
+                        self.results['is_correct'] = False
+                        self.results['state_result'] = TaskResult.Failure
+                        self._logger.info(self.name + f': Incorrect nose poke ({target_num}). Failure.')
 
-        # Nose poke ターゲットの LED を消灯します。
-        self._task_gpio.switch_nose_poke_leds('OFF')
+                    # Nose poke ターゲットの LED を消灯します。
+                    self._task_gpio.switch_nose_poke_leds('OFF')
+                    return
 
-        # limited_hold_in_s で指定された期間において、nose poke 課題を監視します。
-        # stimulus_duration_in_s = -1 の場合は、時間無制限です。
-        start_time = time.perf_counter()
-        while phase_settings.stimulus_duration_in_s == -1 or \
-                time.perf_counter() - start_time < phase_settings.limited_hold_in_s:
-            if self._task_gpio.is_nose_poked:
-                self._task_gpio.get_nose_poke_results(self.results)
-                target_num = self._settings.NOSE_POKE_TARGETS[self.results['selected_index']]
-                self.results['target_num'] = target_num
-                if self.results['selected_index'] in correct_target_index_list:
-                    self.results['is_correct'] = True
-                    self.results['state_result'] = TaskResult.Success
-                    self._logger.info(self.name + f': Correct nose poke ({target_num}). Success.')
-                else:
-                    self.results['is_correct'] = False
-                    self.results['state_result'] = TaskResult.Failure
-                    self._logger.info(self.name + f': Incorrect nose poke ({target_num}). Failure.')
-                return
+                time.sleep(0.001)
 
-            time.sleep(0.001)
+            # Nose poke ターゲットの LED を消灯します。
+            self._task_gpio.switch_nose_poke_leds('OFF')
 
-        self.results['state_result'] = TaskResult.Timeout
-        self._logger.info(self.name + ': Timeout.')
+            # limited_hold_in_s で指定された期間において、nose poke 課題を監視します。
+            # stimulus_duration_in_s = -1 の場合は、時間無制限です。
+            start_time = time.perf_counter()
+            while phase_settings.stimulus_duration_in_s == -1 or \
+                    time.perf_counter() - start_time < phase_settings.limited_hold_in_s:
+                if self._task_gpio.is_nose_poked:
+                    self._task_gpio.get_nose_poke_results(self.results)
+                    target_num = self._settings.NOSE_POKE_TARGETS[self.results['selected_index']]
+                    self.results['target_num'] = target_num
+                    if self.results['selected_index'] in correct_target_index_list:
+                        self.results['is_correct'] = True
+                        self.results['state_result'] = TaskResult.Success
+                        self._logger.info(self.name + f': Correct nose poke ({target_num}). Success.')
+                    else:
+                        self.results['is_correct'] = False
+                        self.results['state_result'] = TaskResult.Failure
+                        self._logger.info(self.name + f': Incorrect nose poke ({target_num}). Failure.')
+                    return
+
+                time.sleep(0.001)
+
+            self.results['state_result'] = TaskResult.Timeout
+            self._logger.info(self.name + ': Timeout.')
+
+    # 報酬を付与します。
+    def _give_reward(self):
+
+        # 報酬用 LED を点灯します。
+        self._task_gpio.switch_reward_led('ON')
+
+        # 報酬用ブザーを鳴らします。
+        self._task_gpio.trigger_reward_buzzer()
+
+        speaker.play_wav("/home/share/Operantbox_development/src/operant_conditioning_task/music/6000Hz_sin_wave.wav")
+
+        # シリンジ ポンプを駆動します。
+        self._task_gpio.trigger_reward_pump()
+
+        # 報酬用 LED の点灯時間を調整します。
+        time.sleep(5)
+
+        # 報酬用 LED を消灯します。
+        self._task_gpio.switch_reward_led('OFF')
+
+        #WAVファイルの停止
+        speaker.stop_wav()
