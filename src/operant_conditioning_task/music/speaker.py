58c5629a77de4c30b95a7be1b667d2d204bac91a
--global <mebuki.numera@gmail.com>
2025-04-28 18:41:01 +0900

    copy music and implement reward

---
+++ src/operant_conditioning_task/music/speaker.py
@@ -0,0 +1,23 @@
+import pygame
+import time
+
+def play_wav(file_path):
+    pygame.mixer.init(frequency=96000)
+    pygame.mixer.music.load(file_path)
+
+    # print(f"Playing {file_path}")
+    pygame.mixer.music.play()
+
+def stop_wav():
+    pygame.mixer.music.stop()
+
+if __name__ == "__main__":
+    # WAVファイルのパスを指定
+    wav_file_path = "6000Hz_sin_wave_96.wav"
+
+    # WAVファイルを再生
+    play_wav(wav_file_path)
+
+    time.sleep(5)
+
+    stop_wav()
---
+++ src/operant_conditioning_task/reward.py
@@ -0,0 +1,21 @@
+import time
+import music.speaker as speaker
+from task_gpio import LedStatus, task_gpio
+
+# 報酬を与える. ブロッキング
+def give_reward():
+    task_gpio.set_reward_led(LedStatus.ON)
+    task_gpio.set_house_led(LedStatus.OFF)
+    task_gpio.set_nose_poke_leds([LedStatus.OFF] * 5)
+
+    speaker.play_wav("music/6000Hz_sin_wave_96.wav")
+    task_gpio.trigger_reward_pump()
+
+    # すべての動作が1秒間続くように待機します。
+    time.sleep(1)
+
+    # 報酬用 LED を消灯します。
+    task_gpio.set_reward_led(LedStatus.OFF)
+
+    # WAVファイルの停止（もし音声ファイルの再生があれば）
+    speaker.stop_wav()
